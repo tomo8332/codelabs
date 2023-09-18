f593fbb6b9c09972f49f2b006560f5703731fb23
TS2648 <suzuki_tomoyuki@retail-ai.jp>
2023-09-18 19:40:26 +0900

    テスト駆動レッスン

--- sources/tdd-go/1_fizzbuzz_start/gopath/src/fizzbuzz/fizzbuzz.go
+++ sources/tdd-go/1_fizzbuzz_start/gopath/src/fizzbuzz/fizzbuzz.go
@@ -1,1 +1,17 @@
+// fizzbuzz.go
+
 package fizzbuzz
+
+import "strconv"
+
+func Convert(n int) string {
+	// これを追加
+	if n%5 == 0 {
+		return "Buzz"
+	}
+
+	if n%3 == 0 {
+		return "Fizz"
+	}
+	return strconv.Itoa(n)
+}
--- sources/tdd-go/1_fizzbuzz_start/gopath/src/fizzbuzz/fizzbuzz_test.go
+++ sources/tdd-go/1_fizzbuzz_start/gopath/src/fizzbuzz/fizzbuzz_test.go
@@ -1,1 +1,29 @@
+// fizzbuzz_test.go
+
 package fizzbuzz_test
+
+import (
+	"testing"
+
+	"github.com/tomo8332/codelabs/sources/tdd-go/1_fizzbuzz_start/gopath/src/fizzbuzz"
+)
+
+func TestConvert(t *testing.T) {
+	tests := []struct {
+		n    int    // 入力値
+		want string // 期待値
+	}{
+		{n: 1, want: "1"},
+		{n: 2, want: "2"},
+		{n: 3, want: "Fizz"},
+		{n: 5, want: "Buzz"},
+	}
+
+	for _, tt := range tests {
+		got := fizzbuzz.Convert(tt.n)
+		if got != tt.want {
+			t.Errorf(`Convert(%v) = %q but want %q`, tt.n, got, tt.want)
+		}
+	}
+
+}
